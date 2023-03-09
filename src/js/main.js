76e168e45c6801d70b776e2463378ff0673ed74b
MiguelAngel <ansonfull@gmail.com>
2023-03-09 01:24:36 +0100

    detalles

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
-// let hamburger = document.querySelector(".hamburger");
-// let menuOpen = document.querySelector(".menuOpen");
+// Loader
+window.addEventListener('load', function () {
+    
+    let hamburger = document.querySelector(".hamburger");
+    let menuOpen = document.querySelector(".menuOpen");
 
-// hamburger.addEventListener('click', function (){
-//     menuOpen.classList.toggle("active");
-// });
+    hamburger.addEventListener('click', function () {
+        menuOpen.classList.toggle("active");
+    });
+
+})
